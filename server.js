76994ac8f486e17c52736373313b716165626139
espython <espython85@gmail.com>
2019-06-18 16:20:37 +0200

    just Finish the backend and start the Initialize the front-end

---
+++ server.js
@@ -0,0 +1,49 @@
+/**
+ * importing our modules
+ */
+import express from 'express';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import passport from 'passport';
+import { config } from 'dotenv';
+import users from './routes/api/users';
+import passportConfig from './config/passport';
+/**
+ * import DotEnv
+ */
+
+config();
+// Dfine our server port
+const port = process.env.PORT || 5000;
+
+const app = express();
+/**
+ * Bodyparser middleware
+ */
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+/**
+ * DB Config
+ */
+const db = process.env.DB_URL;
+// Connect to MongoDB
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log('Mongodb successfully connected'))
+  .catch(err => console.log(err));
+
+/**
+ * Passport config
+ */
+passportConfig(passport);
+/**
+ * Routes
+ */
+app.use('/api/users', users);
+
+/**
+ * Initiallize the server
+ */
+app.listen(port, () => {
+  console.log(`Server up and  running on port ${port} !`);
+});
