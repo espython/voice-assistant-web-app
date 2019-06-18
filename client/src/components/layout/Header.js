010cc85af223e0a3d7d032d088b24488d0bd657e
espython <espython85@gmail.com>
2019-06-19 01:21:12 +0200

    working on front-end

---
+++ client/src/components/layout/Header.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { Link } from "react-router-dom";
+
+export default class Header extends Component {
+    render() {
+        return (
+           
+                <nav>
+                    <h1>Hello from navbar</h1>
+                </nav>
+            
+
+        )
+    }
+}
