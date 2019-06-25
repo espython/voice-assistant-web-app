67dd9a38075e2181949057cd5168b2b2e86ab6bf
espython <espython85@gmail.com>
2019-06-25 07:26:05 +0200

    i hope it will work insha allah

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ import { config } from 'dotenv';
 import { users, posts } from './routes/api';
 import passportConfig from './config/passport';
 /**
- * import DotEnv
+ * import DotEnv nicely
  */
 
 config();
